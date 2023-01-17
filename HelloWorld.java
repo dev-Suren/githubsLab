2d1c3c276969445c2cf7aaf5bf09c3a1d89741ee
dev-suren <surenrana113@gmail.com>
2023-01-17 18:11:36 +0530

    New features added in java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,13 +1,26 @@
 public class HelloWorld{
 	public static void main(String [] args){
-		Hello obj = new Hello();
+		Hello obj = new Hello(24,44,"suren");
 		obj.printingOutput();
+		
 }
 }
 
 class Hello{
+	int a;
+	int b;
+	String c;
+	public void Hello(int a, int b, String c){
+		this.a = a;
+		this.b = b;
+		this.c = c;
+
+}
 	public void printingOutput(){
 		System.out.println("Hello World");
 	}
+
 }
 //This is just a comment
+//this is just a commdt
+
